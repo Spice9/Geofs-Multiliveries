0fb0f4c1f43d3a63e2ac6f03cc9ff079621bd229
Spice_9 <105491758+Spice9@users.noreply.github.com>
2022-05-13 16:03:46 +0530

    Add files via upload

---
+++ Multiliveries.js
@@ -0,0 +1,112 @@
+//Livery-1
+var dropdown = document.createElement("li");
+dropdown.innerHTML = "Boeing 737 Max 8 (Jet Airways)"
+    document.getElementsByClassName("geofs-aircraft-list")[0].appendChild(dropdown);
+    let dropdownSettings = document.createElement("div");
+dropdown.setAttribute("onclick", 'geofs.aircraft.instance.loadLivery("1")');
+
+//Livery-2
+var dropdown2 = document.createElement("li");
+dropdown2.innerHTML = "Boeing 737 Max 8 (FlyDubai)"
+    document.getElementsByClassName("geofs-aircraft-list")[0].appendChild(dropdown2);
+    let dropdown2Settings = document.createElement("div");
+dropdown2.setAttribute("onclick", 'geofs.aircraft.instance.loadLivery("2")');
+
+//Livery-3
+var dropdown3 = document.createElement("li");
+dropdown3.innerHTML = "Boeing 737 Max 8 (Aerolineas Argentinas)"
+    document.getElementsByClassName("geofs-aircraft-list")[0].appendChild(dropdown3);
+    let dropdown3Settings = document.createElement("div");
+dropdown3.setAttribute("onclick", 'geofs.aircraft.instance.loadLivery("3")');
+
+
+//Livery-4
+var dropdown4 = document.createElement("li");
+dropdown4.innerHTML = "Boeing 737 Max 8 (Alaska Airlines)"
+    document.getElementsByClassName("geofs-aircraft-list")[0].appendChild(dropdown4);
+    let dropdown4Settings = document.createElement("div");
+dropdown4.setAttribute("onclick", 'geofs.aircraft.instance.loadLivery("4")');
+
+//Livery-5
+var dropdown5 = document.createElement("li");
+dropdown5.innerHTML = "Boeing 737 Max 8 (Turkish Airlines)"
+    document.getElementsByClassName("geofs-aircraft-list")[0].appendChild(dropdown5);
+    let dropdown5Settings = document.createElement("div");
+dropdown5.setAttribute("onclick", 'geofs.aircraft.instance.loadLivery("5")');
+
+//Livery-6
+var dropdown6 = document.createElement("li");
+dropdown6.innerHTML = "Boeing 737 Max 8 (Smartwings)"
+    document.getElementsByClassName("geofs-aircraft-list")[0].appendChild(dropdown6);
+    let dropdown6Settings = document.createElement("div");
+dropdown6.setAttribute("onclick", 'geofs.aircraft.instance.loadLivery("6")');
+
+//Livery-7
+var dropdown7 = document.createElement("li");
+dropdown7.innerHTML = "Boeing 737 Max 8 (United)"
+    document.getElementsByClassName("geofs-aircraft-list")[0].appendChild(dropdown7);
+    let dropdown7Settings = document.createElement("div");
+dropdown7.setAttribute("onclick", 'geofs.aircraft.instance.loadLivery("7")');
+
+//Livery-8
+var dropdown8 = document.createElement("li");
+dropdown8.innerHTML = "Boeing 737 Max 8 (Malaysia Airlines)"
+    document.getElementsByClassName("geofs-aircraft-list")[0].appendChild(dropdown8);
+    let dropdown8Settings = document.createElement("div");
+dropdown8.setAttribute("onclick", 'geofs.aircraft.instance.loadLivery("8")');
+
+//Livery-9
+var dropdown9 = document.createElement("li");
+dropdown9.innerHTML = "Boeing 737 Max 8 (Vietjet)"
+    document.getElementsByClassName("geofs-aircraft-list")[0].appendChild(dropdown9);
+    let dropdown9Settings = document.createElement("div");
+dropdown9.setAttribute("onclick", 'geofs.aircraft.instance.loadLivery("9")');
+
+//Livery-10
+var dropdown10 = document.createElement("li");
+dropdown10.innerHTML = "Boeing 737 Max 8 (RyanAir)"
+    document.getElementsByClassName("geofs-aircraft-list")[0].appendChild(dropdown10);
+    let dropdown10Settings = document.createElement("div");
+dropdown10.setAttribute("onclick", 'geofs.aircraft.instance.loadLivery("10")');
+
+//Livery-11
+var dropdown11 = document.createElement("li");
+dropdown11.innerHTML = "Boeing 737 Max 8 (Ethiopian)"
+    document.getElementsByClassName("geofs-aircraft-list")[0].appendChild(dropdown11);
+    let dropdown11Settings = document.createElement("div");
+dropdown11.setAttribute("onclick", 'geofs.aircraft.instance.loadLivery("11")');
+
+//Livery-12
+var dropdown12 = document.createElement("li");
+dropdown12.innerHTML = "Boeing 737 Max 8 (Iceland Air)"
+    document.getElementsByClassName("geofs-aircraft-list")[0].appendChild(dropdown12);
+    let dropdown12Settings = document.createElement("div");
+dropdown12.setAttribute("onclick", 'geofs.aircraft.instance.loadLivery("12")');
+
+//Livery-13
+var dropdown13 = document.createElement("li");
+dropdown13.innerHTML = "Boeing 737 Max 8 (Royal Air Maroc)"
+    document.getElementsByClassName("geofs-aircraft-list")[0].appendChild(dropdown13);
+    let dropdown13Settings = document.createElement("div");
+dropdown13.setAttribute("onclick", 'geofs.aircraft.instance.loadLivery("13")');
+
+//Livery-14
+var dropdown14 = document.createElement("li");
+dropdown14.innerHTML = "Boeing 737 Max 8 (Oman Air)"
+    document.getElementsByClassName("geofs-aircraft-list")[0].appendChild(dropdown14);
+    let dropdown14Settings = document.createElement("div");
+dropdown14.setAttribute("onclick", 'geofs.aircraft.instance.loadLivery("14")');
+
+//Livery-15
+var dropdown15 = document.createElement("li");
+dropdown15.innerHTML = "Boeing 737 Max 8 (Flair)"
+    document.getElementsByClassName("geofs-aircraft-list")[0].appendChild(dropdown15);
+    let dropdown15Settings = document.createElement("div");
+dropdown15.setAttribute("onclick", 'geofs.aircraft.instance.loadLivery("15")');
+
+//Livery-16
+var dropdown16 = document.createElement("li");
+dropdown16.innerHTML = "Boeing 737 Max 8 (Garuda Indonesia)"
+    document.getElementsByClassName("geofs-aircraft-list")[0].appendChild(dropdown16);
+    let dropdown16Settings = document.createElement("div");
+dropdown16.setAttribute("onclick", 'geofs.aircraft.instance.loadLivery("16")');
