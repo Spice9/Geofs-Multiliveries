e99b67cffc06c2fad2e64a7072d917b323bd0a26
Ariakim-Taiyo <79466778+Ariakim-Taiyo@users.noreply.github.com>
2022-11-17 07:46:31 -0500

    Update rmu.js

--- rmu.js
+++ rmu.js
@@ -1,6 +1,6 @@
 // index.js
 const Mustache = require('mustache');
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './main.mustache';
 /**
