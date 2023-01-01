69de87977682a93eff80a50d65c5701a3a2d6654
Ariakim-Taiyo <79466778+Ariakim-Taiyo@users.noreply.github.com>
2023-01-01 11:21:14 -0500

    Security and client checking for custom liveries

--- Multiliveries.js
+++ Multiliveries.js
@@ -7,8 +7,19 @@ let buttonDiv = document.createElement("button");
 let element = document.getElementById("mlBttn");
 let message = ""
 let contributors;
-
+let overlayEnabled = false;
 function changeLivery(livery) {
+  if (livery === "inv") {
+    console.log("Invalid client, please use the original code.");
+    return;
+  }
+  if (livery.toString().includes("?custom")) {
+    overlayEnabled = true;
+    geofs.fx.expOverlay.update();
+  } else {
+    overlayEnabled = false;
+    geofs.fx.expOverlay.update();
+  }
   if (livery.toString().includes("https://")) {
     geofs.api.changeModelTexture(geofs.aircraft.instance.definition.parts[0] ["3dmodel"]._model, livery, 0);
   } else {
@@ -39,10 +50,13 @@ buttonDiv.addEventListener("click", function() {
   opened = true;
 });
 document.body.appendChild(buttonDiv);
+
+if (document.getElementsByClassName("fmc-btn").length == 0) {
 document.getElementsByClassName("geofs-ui-bottom")[0].appendChild(buttonDiv);
-
 document.getElementsByClassName("geofs-ui-bottom")[0].insertBefore(element, buttonDiv);
-
+} else {
+  document.getElementsByClassName("fmc-prog-info")[0].appendChild(buttonDiv);
+}
 
 function setupMP() {
   if (geofs.version == 2.9) {
@@ -99,3 +113,48 @@ contributors.forEach(function(e){
 })
 console.log(message)
 }, 1000)
+geofs["overlay.glsl"] = "" + `
+uniform sampler2D colorTexture;
+uniform sampler2D watermark;
+uniform bool isEnabled;
+varying vec2 v_textureCoordinates;
+void main() {
+vec4 color = texture2D(watermark, v_textureCoordinates);
+  if (color.r > 0.0 && isEnabled) {
+    gl_FragColor = mix(texture2D(colorTexture, v_textureCoordinates), color, 0.5);
+  } else {
+  gl_FragColor = texture2D(colorTexture, v_textureCoordinates);
+  }
+}
+`
+geofs.fx.expOverlay = {
+  create: function() {
+    geofs.fx.expOverlay.shader =  new Cesium.PostProcessStage({
+      fragmentShader : geofs["overlay.glsl"], 
+      uniforms: {
+        watermark: "https://138772948-227015667470610340.preview.editmysite.com/uploads/1/3/8/7/138772948/overlay.png",
+        isEnabled: false
+      }
+    })
+    geofs.api.viewer.scene.postProcessStages.add(geofs.fx.expOverlay.shader);
+  },
+  update: function() {
+    geofs.fx.expOverlay.shader.uniforms.isEnabled = overlayEnabled;
+    if (overlayEnabled) mlui.postMessage("valid", "*");
+  }
+};
+geofs.fx.expOverlay.create()
+
+geofs.aircraft.Aircraft.prototype.change = function(a, b, c, d) {
+    overlayEnabled = false;
+    geofs.fx.expOverlay.update();
+    var e = this;
+    a = a || this.aircraftRecord.id;
+    c = this.load(a, this.getCurrentCoordinates(), c, d);
+    c.then(function() {
+        e.loadLivery(b)
+    });
+    geofs.api.analytics.event("aircraft", geofs.aircraftList[a].name);
+    return c
+}
+;
